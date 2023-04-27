168c9abcd450bfab38be4c33097741add2ca72b3
kkkbcsooorichhh <kingblechen8888@Gmail.com>
2023-04-28 01:53:52 +0800

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -265,7 +265,7 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
@@ -290,5 +290,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
